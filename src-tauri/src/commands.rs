c98f0284e0aa20e9389c346ad3256fbfd350ece4
Valter Santos <valter.c.santos@protonmail.com>
2025-02-21 19:46:54 +1300

    style(backend): format adapters info command return type

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -2,7 +2,7 @@
 use crate::wii_pin_generator::{self, BluetoothAdapterInfo};
 
 #[tauri::command]
-pub async fn get_all_bluetooth_adapters_info() -> Result<Vec <Result<BluetoothAdapterInfo, String>>  , String> {
+pub async fn get_all_bluetooth_adapters_info() -> Result<Vec<Result<BluetoothAdapterInfo, String>>,String> {
     match wii_pin_generator::get_all_bluetooth_adapters_info().await {
         Ok(vec) => {
             Ok(vec.into_iter()
