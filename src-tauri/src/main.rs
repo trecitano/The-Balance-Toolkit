ba2112ad263fcb0016497906b21d622c45b07679
Valter Santos <valter.c.santos@protonmail.com>
2025-02-22 17:52:27 +1300

    Temporary commit

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,11 +1,16 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-
 mod wii_pin_generator;
 
 #[tokio::main]
 async fn main() {
-    println!("Pins are {:?}", wii_pin_generator::get_all_bluetooth_adapters_info().await);
+    println!(
+        "Pins are {:?}",
+        wii_pin_generator::get_all_bluetooth_adapters_info().await
+    );
+    wii_pin_generator::all_adapter_bluetooth_connections().await;
+
     //balance_toolkit_app_lib::run()
 }
+
