aeda39e35f3391b18607e3adbb0165e58d369fdb
Valter Santos <valter.c.santos@protonmail.com>
2025-12-27 12:32:49 +1300

    revert(android): revert android cross-compilation attempt
    This reverts commit a8493b8f5fb365386b1c0544eaad303711dc7c9e.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -17,5 +17,21 @@ pub static NINTENDO_BOARD_ID: &str = "Nintendo RVL-WBC-01";
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    file_system::initialize_app_dir()?;
 
+    // Startup: We initialize a single manager that holds all state, and runs in the background.
+    // The architecture of the app is that the commandline or web/tauri send messages to this manager,
+    // and the manager responds via a oneshot channel.
+    let (manager_response_tx, manager_response_rx) = mpsc::channel(100);
+    let manager = ConnectionManager::new(manager_response_tx)?;
+    let manager_command_tx = manager.get_sender_channel();
+    tokio::spawn(async move {
+        if let Err(e) = manager.run().await {
+            eprintln!("Error running connection manager: {}", e);
+            panic!();
+        }
+    });
+    
+    frontend::tauri::initialize(manager_command_tx, manager_response_rx);
+    Ok(())
 }
